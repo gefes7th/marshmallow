fa5eeabe02275447a5168aa5d4d9bd5bbcdd84c2
gefes7th <ggefest@gmail.com>
2025-06-07 13:57:48 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,4 +7,48 @@ COUNT=3
 rm -rf "$DOWNLOAD_DIR"
 mkdir -p "$DOWNLOAD_DIR"
 
-sudo apt-get update
+check_system() {
+    # Get router model
+    MODEL=$(cat /tmp/sysinfo/model)
+    msg "Router model: $MODEL"
+
+    # Check available space
+    AVAILABLE_SPACE=$(df /overlay | awk 'NR==2 {print $4}')
+    REQUIRED_SPACE=15360 # 15MB in KB
+
+    if [ "$AVAILABLE_SPACE" -lt "$REQUIRED_SPACE" ]; then
+        msg "Error: Insufficient space in flash"
+        msg "Available: $((AVAILABLE_SPACE/1024))MB"
+        msg "Required: $((REQUIRED_SPACE/1024))MB"
+        exit 1
+    fi
+
+    if ! nslookup google.com >/dev/null 2>&1; then
+        msg "DNS not working"
+        exit 1
+    fi
+
+    if opkg list-installed | grep -q https-dns-proxy; then
+        msg "Сonflicting package detected: https-dns-proxy. Remove?"
+
+        while true; do
+                read -r -p '' DNSPROXY
+                case $DNSPROXY in
+
+                yes|y|Y|yes)
+                    opkg remove --force-depends luci-app-https-dns-proxy https-dns-proxy luci-i18n-https-dns-proxy*
+                    break
+                    ;;
+                *)
+                    msg "Exit"
+                    exit 1
+                    ;;
+        esac
+    done
+    fi
+
+    if opkg list-installed | grep -q "iptables-mod-extra"; then
+        msg "Found incompatible iptables packages. If you're using FriendlyWrt: https://t.me/itdogchat/44512/181082"
+    fi
+}
+check_system
