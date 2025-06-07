1d1580a2eceb34f832f18a434ee0a8c003504d1b
gefes7th <ggefest@gmail.com>
2025-06-07 14:02:31 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -10,6 +10,11 @@ mkdir -p "$DOWNLOAD_DIR"
 msg() {
     printf "\033[32;1m%s\033[0m\n" "$1"
 }
-msg "In process opkg update"
-opkg update || { echo "opkg update failed"; exit 1; }
-msg "Done opkg update"
+run_main(){
+
+    msg "In process opkg update"
+    opkg update || { echo "opkg update failed"; exit 1; }
+    msg "Done opkg update"
+    
+}
+run_main
