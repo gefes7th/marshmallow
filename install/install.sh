3cb125448a02d20c7f50f5ef3473340a714ff438
gefes7th <ggefest@gmail.com>
2025-06-07 14:42:21 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -11,17 +11,23 @@ msg() {
     printf "\033[32;1m%s\033[0m\n" "$1"
 }
 run_main(){
-
-    msg "In process"
-    #sudo apt update -y && sudo apt upgrade -y || { echo "sudo apt update -y && sudo apt upgrade -y"; exit 1; }
-    #Устанавливаем docker, все приложения будут крутиться в контейнерах 
+	msg "In process"
+	#sudo apt update -y && sudo apt upgrade -y || { echo "sudo apt update -y && sudo apt upgrade -y"; exit 1; }
+	#Устанавливаем docker, все приложения будут крутиться в контейнерах 
 	# Add Docker's official GPG key:
 	sudo apt-get update
 	sudo apt-get install ca-certificates curl
 	sudo install -m 0755 -d /etc/apt/keyrings
 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] 	https://download.docker.com/linux/ubuntu \
+	  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+ 	 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
 
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     msg "Done"
     
 }
