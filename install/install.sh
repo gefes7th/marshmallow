0f410ebca5285d494e49802c614909035520322a
gefes7th <ggefest@gmail.com>
2025-06-07 14:01:06 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,48 +7,9 @@ COUNT=3
 rm -rf "$DOWNLOAD_DIR"
 mkdir -p "$DOWNLOAD_DIR"
 
-check_system() {
-    # Get router model
-    MODEL=$(cat /tmp/sysinfo/model)
-    msg "Router model: $MODEL"
-
-    # Check available space
-    AVAILABLE_SPACE=$(df /overlay | awk 'NR==2 {print $4}')
-    REQUIRED_SPACE=15360 # 15MB in KB
-
-    if [ "$AVAILABLE_SPACE" -lt "$REQUIRED_SPACE" ]; then
-        msg "Error: Insufficient space in flash"
-        msg "Available: $((AVAILABLE_SPACE/1024))MB"
-        msg "Required: $((REQUIRED_SPACE/1024))MB"
-        exit 1
-    fi
-
-    if ! nslookup google.com >/dev/null 2>&1; then
-        msg "DNS not working"
-        exit 1
-    fi
-
-    if opkg list-installed | grep -q https-dns-proxy; then
-        msg "Сonflicting package detected: https-dns-proxy. Remove?"
-
-        while true; do
-                read -r -p '' DNSPROXY
-                case $DNSPROXY in
-
-                yes|y|Y|yes)
-                    opkg remove --force-depends luci-app-https-dns-proxy https-dns-proxy luci-i18n-https-dns-proxy*
-                    break
-                    ;;
-                *)
-                    msg "Exit"
-                    exit 1
-                    ;;
-        esac
-    done
-    fi
-
-    if opkg list-installed | grep -q "iptables-mod-extra"; then
-        msg "Found incompatible iptables packages. If you're using FriendlyWrt: https://t.me/itdogchat/44512/181082"
-    fi
+msg() {
+    printf "\033[32;1m%s\033[0m\n" "$1"
 }
-check_system
+msg "In process opkg update"
+opkg update || { echo "opkg update failed"; exit 1; }
+msg "Done opkg update"
